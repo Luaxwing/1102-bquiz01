b1a2ac9bd752bda161f1e4dad97fc423a9bdd3c0
Luaxwing <nick890111@gmail.com>
2023-12-11 09:28:51 +0800

    php edit.php/跟上課程進度_問題:edit.php套用自己的程式不能正常運作

--- back/mvim.php
+++ back/mvim.php
@@ -1,45 +1,46 @@
 <form method="post" action="./api/edit.php">
-    <table width="100%">
-        <tbody>
-            <tr class="yel">
-                <td width="70%">動畫圖片</td>
-                
-                <td width="10%">顯示</td>
-                <td width="10%">刪除</td>
-                <td></td>
-            </tr>
-            <?php
-            $rows = $Mvim->all();
-            foreach ($rows as $row) {
-                ?>
-                <tr>
-                    <td width="70%">
-                        <img src="../img/<?=$row['img']?>" alt="" style="width=300px;height:150px";>
-                    </td>
-                    
-                    <td width="10%"><input type="checkbox" name="sh[]" id="" value="<?=$row['id'];?>"
-                    <?=($row['sh']==1)?'checked':'';?>></td>
-                    <td width="10%"><input type="checkbox" name="del[]" id="" value="<?=$row['id'];?>"></td>
-                   <td>
-                   <input type="button"onclick="op('#cover','#cvr','./modal/upload.php?table=<?= $do ?>&id=<?=$row['id']?>')" value="更改動畫">
-                   </td>
+        <table width="100%" style="text-align: center">
+            <tbody>
+                <tr class="yel">
+                    <td width="70%">動畫圖片</td>
+                    <td width="10%">顯示</td>
+                    <td width="10%">刪除</td>
+                    <td></td>
                 </tr>
                 <?php
-            }
-            ?>
-        </tbody>
-    </table>
-    <table style="margin-top:40px; width:70%;">
-        <tbody>
-            <tr>
-                <input type="hidden" name="table" id="" value="<?= $do; ?>">
-                <td width="200px"><input type="button"
-                        onclick="op('#cover','#cvr','./modal/<?= $do ?>.php?table=<?= $do ?>')" value="新增動畫圖片">
-                </td>
-                <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
-                </td>
-            </tr>
-        </tbody>
-    </table>
+                $rows=$Mvim->all();
+                foreach($rows as $row){
+                ?>
+                <tr>
+                    <td>
+                        <img src="./img/<?=$row['img'];?>" style="width:150px;height:100px">
+                    </td>
+                    <!--  -->
+                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                    <!--  -->
+                    <td>
+                        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
+                    </td>
+                    <td>
+                    <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                    </td>
+                    <td>
+                    <input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更換動畫">
+                    </td>
+                </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+        <table style="margin-top:40px; width:70%;">
+            <tbody>
+                <tr>
+                    <input type="hidden" name="table" value="<?=$do;?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增動畫圖片"></td>
+                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                </tr>
+            </tbody>
+        </table>
 
-</form>
+    </form>
