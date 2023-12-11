a7e913125e5ea8ca0885afb12632e20479fece6b
Luaxwing <nick890111@gmail.com>
2023-12-11 15:04:38 +0800

    20231211_php edit.php:跟上課堂進度/copy

--- back/bottom.php
+++ back/bottom.php
@@ -1,22 +1,22 @@
-<form method="post"  action="../api/edit_info.php">
-        <table width="50%" style="margin:auto;">
-            <tbody>
-                <tr class="yel">
-                    <td width="50%">頁尾版權</td>
-                    <td width="50%"><input type="text" name="bottom" id="" value="<?=$Bottom->find(1)['bottom']?>"></td>
-                    <input type="hidden" name="table" id="" value="<?=$do;?>">
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-        <table style="margin-top:40px; width:70%;">
-            <tbody>
-                <tr>
-                    <td width="200px">
-                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+<form method="post" action="../api/edit_info.php">
+    <table width="50%" style="margin:auto;">
+        <tbody>
+            <tr class="yel">
+                <td width="50%">頁尾版權</td>
+                <td width="50%"><input type="text" name="bottom" id="" value="<?= $Bottom->find(1)['bottom'] ?>"></td>
+                <input type="hidden" name="table" id="" value="<?= $do; ?>">
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="margin-top:40px; width:70%;">
+        <tbody>
+            <tr>
+                <td width="200px">
+                <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
-    </form>
+</form>
--- back/news.php
+++ back/news.php
@@ -1,57 +1,63 @@
 <form method="post" action="./api/edit.php">
-    <table width="100%">
-        <tbody>
-            <tr class="yel">
-                <!-- <td width="45%">網站標題</td> -->
-                <td width="80%">最新消息資料內容</td>
-                <td width="10%">顯示</td>
-                <td width="10%">刪除</td>
-                <!-- <td></td> -->
-            </tr>
-            <?php
-            $total = $DB->count();
-            $div = 5;
-            $pages = ceil($total / $div);
-            $now = $_GET['p'] ?? 1;
-            $start = ($now - 1) * $div;
-            $rows = $DB->all("limit $start,$div");
-            foreach ($rows as $row) {
-                ?>
-                <tr>
-
-                    <td><textarea type="text" name="text[<?= $row['id'] ?>]" id=""
-                            style="width:90%;height:50px;">"<?= $row['text']; ?></textarea></td>
-                    <td><input type="checkbox" name="sh[]" id="" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
-                    <td><input type="checkbox" name="del[]" id="" value="<?= $row['id']; ?>"></td>
-                    <!-- <td>
-                   <input type="button"onclick="op('#cover','#cvr','./modal/upload.php?table=<?= $do ?>&id=<?= $row['id'] ?>')" value="更新圖片">
-                   </td> -->
+        <table width="100%" style="text-align: center">
+            <tbody>
+                <tr class="yel">
+                    <td width="80%">最新消息資料內容</td>
+                    <td width="10%">顯示</td>
+                    <td width="10%">刪除</td>
                 </tr>
                 <?php
-            }
+                $total=$DB->count();
+                $div=5;
+                $pages=ceil($total/$div);
+                $now=$_GET['p']??1;
+                $start=($now-1)*$div;
+                $rows=$DB->all(" limit $start,$div");
+                foreach($rows as $row){
+                ?>
+                <tr>
+                    <td>
+                        <textarea type="text" name="text[<?=$row['id'];?>]" style="width:90%;height:60px"><?=$row['text'];?></textarea>
+                    </td>
+                    <td>
+                        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
+                    </td>
+                    <td>
+                    <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                    </td>
+                </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+        <div class="cent">
+            
+            <?php
+                if($now>1){
+                    $prev=$now-1;
+                    echo "<a href='?do=$do&p=$prev'> < </a>";
+                }
+
+                for($i=1;$i<=$pages;$i++){
+                    $fontsize=($now==$i)?'24px':'16px';
+                    echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'> $i </a>";
+                }
+
+                if($now<$pages){
+                    $next=$now+1;
+                    echo "<a href='?do=$do&p=$next'> > </a>";
+                }
             ?>
-        </tbody>
-    </table>
+        </div>
+        <table style="margin-top:40px; width:70%;">
+            <tbody>
+                <tr>
+                    <input type="hidden" name="table" value="<?=$do;?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增最新消息資料"></td>
+                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                </tr>
+            </tbody>
+        </table>
 
-    <div class="cent">
-        <?php
-        pegetabs($now, $pages,$_GET['do']);
-        ?>
-    </div>
-
-    <table style="margin-top:40px; width:70%;">
-        <tbody>
-            <tr>
-                <input type="hidden" name="table" id="" value="<?= $do; ?>">
-                <td width="200px"><input type="button"
-                        onclick="op('#cover','#cvr','./modal/<?= $do ?>.php?table=<?= $do ?>')" value="新增最新消息資料">
-                    <!-- ajax  -->
-                    <!-- 不是include值要再傳一次 -->
-                </td>
-                <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
-                </td>
-            </tr>
-        </tbody>
-    </table>
-
-</form>
+    </form>
