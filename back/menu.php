923ab35ebe4486b3fc0ae56ba6acb4466fd6f84a
Luaxwing <nick890111@gmail.com>
2023-12-11 16:00:27 +0800

    20231211_php sub.php:modal

--- back/menu.php
+++ back/menu.php
@@ -1,48 +1,53 @@
 <form method="post" action="./api/edit.php">
-        <table width="100%" style="text-align: center">
-            <tbody>
-                <tr class="yel">
-                    <td width="30%">主選單名稱</td>
-                    <td width="30%">選單連結網址</td>
-                    <td width="10%">次選單數</td>
-                    <td width="10%">顯示</td>
-                    <td width="10%">刪除</td>
-                    <td></td>
-                </tr>
-                <?php
+    <table width="100%" style="text-align: center">
+        <tbody>
+            <tr class="yel">
+                <td width="30%">主選單名稱</td>
+                <td width="30%">選單連結網址</td>
+                <td width="10%">次選單數</td>
+                <td width="10%">顯示</td>
+                <td width="10%">刪除</td>
+                <td></td>
+            </tr>
+            <?php
 
-                $rows=$DB->all();
-                foreach($rows as $row){
+            $rows = $DB->all();
+            foreach ($rows as $row) {
                 ?>
                 <tr>
                     <td>
-                        <input type="text" name="text[]"  value="<?=$row['text'];?>">
+                        <input type="text" name="text[]" value="<?= $row['text']; ?>">
                     </td>
                     <td>
-                        <input type="text" name="href[]" value="<?=$row['href'];?>">
+                        <input type="text" name="href[]" value="<?= $row['href']; ?>">
                     </td>
                     <td></td>
                     <td>
-                    <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
+                        <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>>
                     </td>
                     <td>
-                    <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                        <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
                     </td>
+                    <td>
+                        <input type="button" value="編輯次選單"
+                            onclick="op('#cover','#cvr','./modal/submenu.php?table=<?= $do; ?>')">
+                    </td> 
                 </tr>
-                <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
                 <?php
-                }
-                ?>
-            </tbody>
-        </table>
-        <table style="margin-top:40px; width:70%;">
-            <tbody>
-                <tr>
-                    <input type="hidden" name="table" value="<?=$do;?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增主選單"></td>
-                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
-                </tr>
-            </tbody>
-        </table>
+            }
+            ?>
+        </tbody>
+    </table>
+    <table style="margin-top:40px; width:70%;">
+        <tbody>
+            <tr>
+                <input type="hidden" name="table" value="<?= $do; ?>">
+                <td width="200px"><input type="button"
+                        onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增主選單"></td>
+                <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+            </tr>
+        </tbody>
+    </table>
 
-    </form>
+</form>
