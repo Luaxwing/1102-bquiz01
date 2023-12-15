f2311121565cd1e3249a929813f4c65d8b09f329
Luaxwing <nick890111@gmail.com>
2023-12-15 16:28:38 +0800

    20231215:main.php/更多最新消息區/hover

--- front/main.php
+++ front/main.php
@@ -34,7 +34,7 @@
 				echo "<li>";
 				echo mb_substr($n['text'], 0, 20);
 				// 
-				echo "<div class='all' style='display: none;';>";
+				echo "<div class='all' style='display: none;'>";
 				echo "'''";
 				echo $n['text'];
 				echo "</div>";
@@ -42,15 +42,26 @@
 				echo "...";
 				echo "</li>";
 			}
-
-
 			?>
-
-
 		</ul>
 		<div id="altt"
 			style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
 		</div>
+
+		<script>
+			$(".ssaa li").hover(
+				function () {
+					$("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
+					$("#altt").show()
+					// 用得到
+				}
+			)
+			$(".ssaa li").mouseout(
+				function () {
+					$("#altt").hide()
+				}
+			)
+		</script>
 		<!--  -->
 
 		<script>
@@ -100,20 +111,7 @@
 		</script>
 
 
-		<script>
-			$(".ssaa li").hover(
-				function () {
-					$("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
-					// $("#altt").show()
-					// 用不到
-				}
-			)
-			$(".ssaa li").mouseout(
-				function () {
-					$("#altt").hide()
-				}
-			)
-		</script>
+
 
 	</div>
 </div>
